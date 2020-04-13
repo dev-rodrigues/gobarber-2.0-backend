e5f39bc518a00a02eec3097491abc94207035963
CARLOS HENRIQUE DOS SANTOS RODRIGUES JUNIOR <carlos.rjunior@al.infnet.edu.br>
2020-04-13 17:01:08 -0300

    post: create appointment

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import routes from './routes';
 
 const app = express();
+app.use(bodyParser.json());
 app.use(routes);
 
 app.listen(3333, () => {
-	console.log('Server started on port 3333 =)');
+    // eslint-disable-next-line no-console
+    console.log('Server started on port 3333 =)');
 });
