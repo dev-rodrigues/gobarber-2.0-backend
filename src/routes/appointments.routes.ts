6ccfb3e571058917c241220e7cd6c1546f691af6
CARLOS HENRIQUE DOS SANTOS RODRIGUES JUNIOR <carlos.rjunior@al.infnet.edu.br>
2020-04-14 09:39:58 -0300

    validando datas

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,17 +1,36 @@
-import { Router } from 'express';
+import { Router, response } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentRouter = Router();
 
-const appointments = [];
+interface Appointment {
+    id: string;
+    provider: string;
+    date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentRouter.post('/', (req, res) => {
     const { provider, date } = req.body;
 
+    const parsedDate = startOfHour(parseISO(date));
+
+    const findAppointmentInSameDate = appointments.find(appointment =>
+        isEqual(parsedDate, appointment.date),
+    );
+
+    if (findAppointmentInSameDate) {
+        return res
+            .status(400)
+            .json({ message: 'This appointment is already booked' });
+    }
+
     const appointment = {
         id: uuid(),
         provider,
-        date,
+        date: parsedDate,
     };
 
     appointments.push(appointment);
