05b8da6483dd179af96e014a19838bd1d7b259a8
CARLOS HENRIQUE DOS SANTOS RODRIGUES JUNIOR <carlos.rjunior@al.infnet.edu.br>
2020-04-18 22:25:29 -0300

    upload image

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
 import CreateUserService from '../service/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+import uploadConfig from '../config/upload';
 
 const userRouter = Router();
+const upload = multer(uploadConfig);
 
 userRouter.post('/', async (req, res) => {
     try {
@@ -17,5 +22,15 @@ userRouter.post('/', async (req, res) => {
         return res.status(400).json({ error: err.message });
     }
 });
+
+userRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (req, res) => {
+        console.log(req.file);
+        return res.json({ ok: true });
+    },
+);
 
 export default userRouter;
