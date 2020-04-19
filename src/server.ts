c444bd257fc4ce2ddb3ac8eca8558a753af5980c
CARLOS HENRIQUE DOS SANTOS RODRIGUES JUNIOR <carlos.rjunior@al.infnet.edu.br>
2020-04-19 17:09:35 -0300

    liberando acesso das imagens

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,18 @@ import 'reflect-metadata';
 import express from 'express';
 import bodyParser from 'body-parser';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 import './database';
 
 const app = express();
 app.use(bodyParser.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
     // eslint-disable-next-line no-console
-    console.log('Server started on port 3333 =)');
+    console.log('-------------------------------------------------');
+    console.log('---------- SERVER STARTED ON PORT 3333 ----------');
+    console.log('-------------------------------------------------');
 });
